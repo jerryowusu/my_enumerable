a3ff0dd69fa97e3d2ba5241c11d9b09f08c79c13
jerryowusu <jerryowusu6133@gmail.com>
2022-05-06 01:21:29 +0000

    fix linter errors

--- main.rb
+++ main.rb
@@ -4,12 +4,12 @@ require_relative 'my_list'
 list = MyList.new(1, 2, 3, 4)
 
 # Test #all?
-list.all? { |e| e < 5 }
-list.all? { |e| e > 5 }
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
 
 # Test #any?
-list.any? { |e| e == 2 }
-list.any? { |e| e == 5 }
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
 
 # Test #filter
-list.filter { |e| e.even? }
+puts(list.filter(&:even?))
